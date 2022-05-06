f12ae5de10a4520e13280fb5f992f8adb1b3a6f3
Adryjann <adr.bugaj@gmail.com>
2022-05-06 21:56:50 +0200

    r

---
+++ lab-4-zadanie/lab-4-zadanie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_4_zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
